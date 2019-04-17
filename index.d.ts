33b578f23c9e232e38e36c3e45c6957057b9e9ce
ryan0000 <rlibed@esri.com>
2019-04-17 14:51:06 -0700

    Added arcgis js api typings and index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,44 @@
+declare module "Components/Share/Share" {
+  const Share: any;
+  export = Share;
+}
+
+declare module "Components/Share/Share/ShareViewModel" {
+  const ShareViewModel: any;
+  export = ShareViewModel;
+}
+
+declare module "Components/Share/Share/ShareFeatures" {
+  const ShareFeatures: any;
+  export = ShareFeatures;
+}
+
+declare module "Components/Share/Share/ShareItem" {
+  const ShareItem: any;
+  export = ShareItem;
+}
+
+declare module "Components/Screenshot/Screenshot" {
+  const Screenshot: any;
+  export = Screenshot;
+}
+
+declare module "Components/Screenshot/Screenshot/ScreenshotViewModel" {
+  const ScreenshotViewModel: any;
+  export = ScreenshotViewModel;
+}
+
+declare module "Components/Info/Info" {
+  const Info: any;
+  export = Info;
+}
+
+declare module "Components/Info/Info/InfoItem" {
+  const InfoItem: any;
+  export = InfoItem;
+}
+
+declare module "Components/Info/Info/InfoViewModel" {
+  const InfoViewModel: any;
+  export = InfoViewModel;
+}
