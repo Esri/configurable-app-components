c730136caaf95946c2f6a331189f7228abe5c918
ryan0000 <rlibed@esri.com>
2021-05-19 14:06:21 -0700

    Added Page component

---
+++ Page/Page.tsx
@@ -0,0 +1,186 @@
+import { messageBundle, tsx } from "esri/widgets/support/widget";
+import { subclass, property } from "esri/core/accessorSupport/decorators";
+import Widget = require("esri/widgets/Widget");
+import { whenTrueOnce } from "esri/core/watchUtils";
+
+const base = "esri-page";
+
+const CSS = {
+  base,
+  title: `${base}__title-text`,
+  subtitle: `${base}__subtitle-text`,
+  textContainer: `${base}__text-container`,
+  scrollContainer: `${base}__scroll-container`,
+  scrollText: `${base}__scroll-text`,
+  backToCoverPage: `${base}__back-to-cover-page`
+};
+
+interface BackgroundImageStyles {
+  backgroundImage: string;
+  backgroundSize: string;
+}
+
+@subclass("Page")
+class Page extends Widget {
+  constructor(props: any) {
+    super(props);
+  }
+
+  @property()
+  showScrollTop = true;
+
+  @property()
+  title = null;
+
+  @property()
+  titleColor = null;
+
+  @property()
+  subtitle = null;
+
+  @property()
+  subtitleColor = null;
+
+  @property()
+  background = null;
+
+  @property()
+  buttonText = null;
+
+  @property()
+  @messageBundle(
+    "node_modules/@esri/configurable-app-components/Page/t9n/resources"
+  )
+  messages = null;
+
+  postInitialize() {
+    this._handleDefaultMessages();
+    this._handleDocBodyStyles();
+    this._addPageToBody();
+    this.own([
+      whenTrueOnce(this, "showScrollTop", () => {
+        this._handleShowScrollTop();
+      })
+    ]);
+  }
+
+  destroy() {
+    document.body.style.overflow = "";
+    document.body.style.position = "";
+    document.body.style.top = "0";
+    document.body.style.transition = "";
+  }
+
+  render() {
+    const textContainer = this._renderTextContainer();
+    const scrollContainer = this._renderScrollContainer();
+    return (
+      <div
+        class={CSS.base}
+        styles={
+          this.background?.backgroundType === "image"
+            ? this._getBackgroundStyles()
+            : {
+                backgroundColor: this.background?.backgroundColor
+                  ? this.background?.backgroundColor
+                  : "#0079c1"
+              }
+        }
+      >
+        {textContainer}
+        {scrollContainer}
+      </div>
+    );
+  }
+
+  private _getBackgroundStyles(): BackgroundImageStyles {
+    const { backgroundImage } = this.background;
+    const backgroundImageVal = backgroundImage?.url
+      ? `url('${backgroundImage?.url}')`
+      : "none";
+    return {
+      backgroundImage: backgroundImageVal,
+      backgroundSize: "cover"
+    };
+  }
+
+  private _renderTextContainer() {
+    const { title, titleColor, subtitle, subtitleColor } = this;
+    return (
+      <div class={CSS.textContainer}>
+        <h1 class={CSS.title} style={{ color: titleColor }}>
+          {title}
+        </h1>
+        <span class={CSS.subtitle} style={{ color: subtitleColor }}>
+          {subtitle}
+        </span>
+      </div>
+    );
+  }
+
+  private _renderScrollContainer() {
+    return (
+      <div class={CSS.scrollContainer}>
+        <button
+          onclick={this._handleScroll.bind(this)}
+          aria-label={this.buttonText}
+          title={this.buttonText}
+        >
+          <span class={CSS.scrollText}>{this.buttonText}</span>
+          <calcite-icon icon="chevron-down" scale="l" />
+        </button>
+      </div>
+    );
+  }
+
+  private _addPageToBody(): void {
+    document.body.insertBefore(
+      this.container as HTMLElement,
+      document.body.childNodes[0]
+    );
+  }
+
+  private _handleDefaultMessages(): void {
+    const { title, subtitle, buttonText } = this.messages;
+    if (!this.title) {
+      this.title = title;
+    }
+    if (!this.subtitle) {
+      this.subtitle = subtitle;
+    }
+    if (!this.buttonText) {
+      this.buttonText = buttonText;
+    }
+  }
+
+  private _handleDocBodyStyles(): void {
+    document.body.style.overflow = "hidden";
+    document.body.style.position = "relative";
+    document.body.style.top = "0";
+    document.body.style.transition = "top 0.5s ease 0s";
+  }
+
+  private _handleShowScrollTop(): void {
+    const fabElement = document.createElement("calcite-fab") as any;
+    fabElement.classList.add(CSS.backToCoverPage);
+    fabElement.icon = "chevron-up";
+    fabElement.textEnabled = true;
+    const { backToCoverPage } = this.messages;
+    fabElement.label = backToCoverPage;
+    fabElement.title = backToCoverPage;
+    fabElement.onclick = () => {
+      this._scrollBackToCoverPage();
+    };
+    document.body.appendChild(fabElement);
+  }
+
+  private _scrollBackToCoverPage(): void {
+    document.body.style.top = "0";
+  }
+
+  private _handleScroll(): void {
+    document.body.style.top = "-100%";
+  }
+}
+
+export = Page;
---
+++ Page/README.md
@@ -0,0 +1,68 @@
+# Page
+
+### Constructor:
+
+#### new **Page(_properties?_)**
+
+##### Property Overview:
+
+| Name          | Type       | Summary                                      |
+| ------------- | ---------- | -------------------------------------------- |
+| title         | String     | Title text.                                  |
+| titleColor    | String     | Title color.                                 |
+| subtitle      | String     | Subtitle text.                               |
+| subtitleColor | String     | Title color.                                 |
+| background    | Background | Background color or image.                   |
+| buttonText    | String     | Button text.                                 |
+| showScrollTop | Boolean    | Display button to scroll back to cover page. |
+
+#### Interfaces:
+
+```
+interface Background {
+    backgroundType: string;
+    backgroundImage: ImageFile;
+    backgroundColor: string;
+}
+
+interface ImageFile {
+    name: string;
+    size: number;
+    type: string;
+    url: string;
+}
+```
+
+### **Example:**
+
+```
+const page = new Page({
+    container: document.createElement("esri-page"),
+    title: "Title",
+    titleColor: "#ffffff",
+    subtitle: "Subtitle",
+    subtitleColor: "#ffffff",
+    textPosition: "center-leading",
+    buttonText: "Explore",
+    background: {
+      backgroundType: "color",
+      backgroundImage: null,
+      backgroundColor: "#0079c1"
+    }
+});
+```
+
+Load language files with t9n message bundler in app with `"esri/intl".registerMessageBundleLoader()` and `"esri/intl".createJSONLoader()`.
+
+```
+registerMessageBundleLoader(
+  createJSONLoader({
+    pattern: "node_modules/@esri/configurable-app-components/",
+    base: "node_modules/@esri/configurable-app-components",
+    location: new URL(
+      "../node_modules/@esri/configurable-app-components/Page",
+      window.location.href
+    )
+  })
+);
+```
